a7773b9d64bf3c4623395a1ee07b402892fcdf43
Lucas Andrade <lucasandradeecomp@outlook.com>
2021-03-03 11:01:02 -0300

    Pull 03/03/2021 - 11:00

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -54,5 +54,12 @@ namespace SistemaToque.Controllers
 
             return View();
         }
+
+        public ActionResult Teste()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
     }
 }
