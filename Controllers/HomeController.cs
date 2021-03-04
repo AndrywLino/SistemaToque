807b1cfb9d0a7cabe6cd522f29ab0ff7d75726a6
junkflash30 <32003079+junkflash30@users.noreply.github.com>
2021-03-04 13:22:54 -0300

    Toques

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -127,14 +127,6 @@ namespace SistemaToque.Controllers
                 {
                     pla.DiaSemana = pla.DiaSemana + "- Domingo ";
                 }
-                if(pla.Canal == 1)
-                {
-                    pla.TxCanal = "P2";
-                }
-                if (pla.Canal == 2)
-                {
-                    pla.TxCanal = "HDMI";
-                }
                 if (pla.NivelEnsino == 1)
                 {
                     pla.TxEnsino = "Fund 1";
@@ -152,6 +144,14 @@ namespace SistemaToque.Controllers
             }
             return planilha;
         }
+
+        [HttpGet]
+        public ActionResult EditarToque(string arquivo)
+        {
+            string toq = arquivo;
+            return View();
+        }
+
 
     }
 }
