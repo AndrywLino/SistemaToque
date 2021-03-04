629756c5d2e325e6707c0efa753d31d8608c47ae
junkflash30 <32003079+junkflash30@users.noreply.github.com>
2021-03-04 08:39:31 -0300

    toques

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -69,6 +69,7 @@ namespace SistemaToque.Controllers
 
         public ActionResult Toque(bool logado = false)
         {
+            logado = true;
             if (logado)
             {
                 ViewBag.Message = "Your contact page.";
